25eaa5ce665c2f34b376d7fbc8065c47b1e64a50
Carlo Di Maio <carlo.dimaio@gmail.com>
2017-02-11 23:24:31 +0100

    header component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+
+import Header from './commn/components/header/Header';
 import Footer from './common/components/footer';
 import Jumbotron from './common/components/jumbotron';
+
+
 
 class App extends Component {
   render() {
     return (
+
       <div className="App container-fluid">
 
-          <div className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h2>Welcome to React</h2>
-          </div>
+          <Header />
 
           <p className="App-intro">
               To get started, edit <code>src/App.js</code> and save to reload.
           </p>
-
-
           <Jumbotron />
 
           <Footer />
 
       </div>
+
     );
   }
 }
---
+++ src/common/components/header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+
+
+
+class Header extends React.Component {
+
+  render() {
+    return (
+      <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>New Header Welcome to React</h2>
+      </div>
+    );
+  }
+
+}
+
+export default Header;
