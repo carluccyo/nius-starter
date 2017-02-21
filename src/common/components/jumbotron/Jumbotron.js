bcbf58ccf453b4b3b04a618c423bbe0335cda454
Carlo Di Maio <carlo.dimaio@gmail.com>
2017-02-21 15:37:31 +0100

    deploy message

--- src/common/components/jumbotron/Jumbotron.js
+++ src/common/components/jumbotron/Jumbotron.js
@@ -6,6 +6,7 @@ class Jumbotron extends React.Component {
     return (
       <div className="jumbotron">
           <h1>Theme example</h1>
+          <h2>Theme example</h2>
           <p>This is a template showcasing the optional theme stylesheet included in Bootstrap. Use it as a starting point to create something more unique by building on or modifying it.</p>
       </div>
     );
