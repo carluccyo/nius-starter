2171e164dd87528aca251c92184b8abbe61d275f
Carlo Di Maio <carlo.dimaio@gmail.com>
2017-02-21 11:59:12 +0100

    no msg

--- src/common/components/footer/footer.js
+++ src/common/components/footer/footer.js
@@ -12,9 +12,7 @@ class Footer extends React.Component {
     return (
       <footer className="blog-footer">
           <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-          <p>
-              <a href="#">Back to top</a>
-          </p>
+          {/* <p><a href="#">Back to top</a></p> */}
       </footer>
 
     );
--- src/common/components/header/Header.js
+++ src/common/components/header/Header.js
@@ -9,13 +9,37 @@ class Header extends React.Component {
 
   render() {
     return (
-      <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>New Header Welcome to React</h2>
+      <div id="main-nav" className="navbar bs-docs-nav fixed" role="banner">
+          <div className="container">
+              <div className="navbar-header responsive-logo">
+                  <button className="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-navbar-collapse" aria-expanded="false">
+                      <span className="sr-only">Toggle navigation</span>
+                      <span className="icon-bar"></span>
+                      <span className="icon-bar"></span>
+                      <span className="icon-bar"></span>
+                  </button>
+                  <div className="navbar-brand">
+                      {/* <img src="images/logo.png" alt="Zerif" /> */}
+                  </div>
+              </div>
+              <nav className="navbar-collapse collapse" role="navigation" id="bs-navbar-collapse" aria-expanded="false" >
+                  <ul className="nav navbar-nav navbar-right responsive-nav main-nav-list">
+                      <li className="current"><a href="#home">Home</a></li>
+                      <li className=""><a href="#focus">Focus</a></li>
+                      <li className=""><a href="#works">Works</a></li>
+                      <li className=""><a href="#aboutus">About Us</a></li>
+                      <li className=""><a href="#team">Team</a></li>
+                      <li><a href="#pricingtable">Pricing</a></li>
+                      <li><a href="#features">Features</a></li>
+                      <li><a href="#products">Products</a></li>
+                      <li><a href="#testimonials">Testimonials</a></li>
+                      <li><a href="#contact">Contact</a></li>
+                  </ul>
+              </nav>
+          </div>
       </div>
-    );
-  }
-
+      );
+    }
 }
 
 export default Header;
