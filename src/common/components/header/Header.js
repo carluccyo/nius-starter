08df168a41a7f37034ae2829b57dba65d81ddc3a
Carlo Di Maio <carlo.dimaio@gmail.com>
2017-02-25 23:51:50 +0100

    build

--- src/common/components/header/Header.js
+++ src/common/components/header/Header.js
@@ -13,7 +13,7 @@ class Header extends React.Component {
 
   render() {
     return (
-      <nav className="navbar navbar-default blu-navbar navbar-static-top">
+      <nav className="navbar navbar-default navbar-static-top">
           <div className="container">
               <div className="navbar-header">
                   <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
